e792c8d108994af9aeb58214d141412823a444c4
farrellkj2 <farrellkj2@gmail.com>
2018-04-12 18:23:03 -0400

    update chemistry script to complete skeleton for data; note flags (##!!) by KF that need clarification

--- Scripts/Chemistry_aggregation.R
+++ Scripts/Chemistry_aggregation.R
@@ -1,13 +1,25 @@
 # Script to pull in water chemistry data from multiple reservoirs and years ####
-# Collate into one file for EDI 
 
-#### Load packages, read in data files ####
 #install.packages('pacman')
 pacman::p_load(tidyverse, lubridate)
 
 # Load .csv files whose file names end in _Chemistry
-## Need to mutate columns to correct parsing; differences between files prevented
-## correct auto-parsing
-raw <- dir(path = "./Data", pattern = "*_Chemistry.csv") %>% #
+raw <- dir(path = "./Data", pattern = "*_Chemistry.csv") %>% 
   map_df(~ read_csv(file.path(path = "./Data", .), col_types = cols(.default = "c"))) %>%
-  mutate(Year = as.integer(Year))
+  select(-Year, -(DIC_mgL:CH4_ppm), -(`OI TOC Conc (ppm)`:`Vario DNb Mean of 3 reps`)) %>%
+  rename(Depth_m = Depth, DateTime = Date) %>%
+  mutate(DateTime = ymd_hms(DateTime), Site = as.factor(50), Chla_mgL = "", 
+         Turbidity_NTU = "", Conductivity_uScm = "", PAR_umolm2s = "",
+                ORP = "", pH = "") %>% group_by(Reservoir, Site, DateTime, Notes) %>%
+  mutate_if(is.character,as.double) 
+
+chemistry <- raw %>% 
+  select(Reservoir, Site, DateTime, Depth_m, TN_ugL, TP_ugL, 
+         NH4_ugL, NO3NO2_ugL, NO3_ugL, SRP_ugL, PO4_ugL, DOC_mgL, Chla_mgL, Turbidity_NTU, 
+         Conductivity_uScm, PAR_umolm2s, ORP, pH, Notes) %>%
+  arrange(DateTime, Reservoir) # %>%
+#write_csv('./SUBFOLDER?/chemistry.csv')
+
+##!! Flags from KF: are NO3_ugL values == NO3NO2_ugL or was SOP different? Ditto PO4_ugL == SRP_ugL? 
+##!! Do we retain a "notes" column in EDI version? If not, should hand-check these caveats before final version
+##!! Some nutrient concentrations <0 but retained... should check! 
