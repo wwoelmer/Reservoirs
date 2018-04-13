122682174dcc433ffe31227c5491010631b54076
farrellkj2 <farrellkj2@gmail.com>
2018-04-12 21:02:03 -0400

    add ability to use NO3_ugL or PO4_ugL column from FCR 2013; pull and include additional reservoirs

--- Scripts/Chemistry_aggregation.R
+++ Scripts/Chemistry_aggregation.R
@@ -13,16 +13,20 @@ chemistry <- raw_chem %>%
          -(`OI DOC Conc (ppm)`:`Vario DNb Mean of 3 reps`)) %>%
   rename(Depth_m = Depth, DateTime = Date) %>%
   mutate(DateTime = ymd_hms(DateTime)) %>%
-  mutate(Site = ifelse(Depth_m != "999", "50", "100")) %>%
- #mutate(Depth_m = recode(Depth_m, "999" = "100")) %>% ##!! recode inflow as "100"-- currently not working
-  group_by(Reservoir, Site, DateTime, Notes) %>% # columns not to parse to double
-  mutate_if(is.character,as.double) %>%  # parse all other columns to double
+  group_by(Reservoir, DateTime, Notes) %>% # columns not to parse to numeric
+  mutate_if(is.character,funs(round(as.double(.), 2))) %>%  # parse all other columns to numeric
+  mutate(NO3NO2_ugL = ifelse(is.na(NO3NO2_ugL), NO3_ugL, NO3NO2_ugL),  # move NO3_ugL values to NO3NO2 column
+         SRP_ugL = ifelse(is.na(SRP_ugL), PO4_ugL, SRP_ugL)) %>% # move PO4 values to SRP column
+  mutate(Site = ifelse(Depth_m != 999, 50, 100)) %>% # Add site ID; 50 = deep hole; 100 = inflow
+  mutate(Depth_m = replace(Depth_m, Depth_m == 999, 100)) %>% ##!! What should Depth_m be for inflow??
   select(Reservoir, Site, DateTime, Depth_m, TN_ugL, TP_ugL, infTPloads_g, # reorder columns by name
-         NH4_ugL, NO3NO2_ugL, NO3_ugL, SRP_ugL, PO4_ugL, DOC_mgL, Notes) %>%
-  arrange(DateTime, Reservoir) # %>%
-#write_csv('./SUBFOLDER?/chemistry.csv')
+         NH4_ugL, NO3NO2_ugL, SRP_ugL, DOC_mgL, Notes) %>%
+  arrange(DateTime, Reservoir, Depth_m) 
 
-##!! Flags from KF: are NO3_ugL values == NO3NO2_ugL or was SOP different? Ditto PO4_ugL == SRP_ugL? 
+# Write to CSV (using write.csv for now; want ISO format embedded?)
+write.csv('./Formatted_Data/chemistry.csv', row.names=F)
+
+##!! Check reasonable precision of nutrient concentrations for reporting; KF rounded NO3 and PO4 to 2 decimal(?)
 ##!! Do we retain a "Notes" column in EDI version? If not, should hand-check these caveats before final version
 ##!! Some nutrient concentrations <0 but retained... should check! 
 
