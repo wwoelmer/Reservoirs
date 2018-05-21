049d6b0c889bc9d381743149323d514c73d842c8
farrellkj2 <farrellkj2@gmail.com>
2018-05-21 14:14:53 -0400

    committing (slightly) old but still relevant Matched Dates script and files

--- Scripts/Chemistry_YSI_PAR_matchedDates.R
+++ Scripts/Chemistry_YSI_PAR_matchedDates.R
@@ -1,18 +1,37 @@
-# Check for missing dates between Chemistry and YSI files
+# Check for missing dates between Chemistry, YSI, Secchi files
 pacman::p_load(tidyverse, lubridate)
 
 chemistry <- read_csv('./Formatted_Data/chemistry.csv') %>%
   mutate(Chemistry = "Yes", 
          DateTime = as.Date(DateTime)) %>%
-  select(Reservoir:DateTime, Chemistry)
+  group_by(Reservoir, Site, DateTime) %>%
+  distinct(Reservoir, Site, DateTime, Depth_m, .keep_all = T) %>%
+  select(Reservoir:Depth_m, Chemistry)
+
+Secchi <- read_csv('./Formatted_Data/Secchi_depth.csv') %>%
+  mutate(Secchi = "Yes",
+         DateTime = as.Date(DateTime)) %>%
+  select(Reservoir:DateTime, Secchi)
 
 YSI <- read_csv('./Formatted_Data/YSI_PAR_profiles.csv') %>%
-  mutate(DateTime = as.Date(DateTime)) %>%
-  select(-Depth_m)
+  mutate(DateTime = as.Date(DateTime),
+         Temp = ifelse(!is.na(Temp_C), "Yes", "No"),
+         DO = ifelse(!is.na(DO_mgL), "Yes", "No"),
+         DOsat = ifelse(!is.na(DOSat), "Yes", "No"),
+         Cond = ifelse(!is.na(Cond_uScm), "Yes", "No"),
+         PAR = ifelse(!is.na(PAR_umolm2s), "Yes", "No"),
+         ORP = ifelse(!is.na(ORP), "Yes", "No"),
+         PH = ifelse(!is.na(pH), "Yes", "No")) %>%
+  select(Reservoir:pH, Notes)
 
-all <- right_join(YSI, chemistry) %>% 
-  group_by(Reservoir, DateTime, Chemistry) %>%
-  distinct(Reservoir, DateTime, .keep_all= T) %>%
-  select(Chemistry, Reservoir, DateTime:Notes) %>%
-  arrange(Reservoir, DateTime) %>%
-  write_csv('./Formatted_Data/Chemistry_YSI_PAR_matchedDates.csv')
+all <- full_join(YSI, chemistry) %>% 
+  full_join(., Secchi) %>%
+  mutate(Chemistry = ifelse(!is.na(Chemistry), Chemistry, "No"),
+         Secchi = ifelse(!is.na(Secchi), Secchi, "No")) %>%
+  group_by(Reservoir, Site, DateTime) %>%
+  #distinct(Reservoir, Site, DateTime, Depth_m, .keep_all= T) %>%
+  select(Reservoir:Depth_m, Secchi, Chemistry, Temp_C:pH, Notes) %>%
+  arrange(Reservoir, Site, DateTime, Depth_m) %>%
+  #mutate_at(.vars = vars(Secchi:pH),
+  #          .funs = funs(ifelse(is.na(.),"No", .)))%>%
+  write_csv('./Formatted_Data/Chemistry_YSI_PAR_Secchi_matchedDates.csv')
