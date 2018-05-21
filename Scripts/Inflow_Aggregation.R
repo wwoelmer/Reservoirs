67d50eb9db316f9289bc1eb90b89e71fbd7beed0
bethanybookout <38041971+bethanybookout@users.noreply.github.com>
2018-05-21 08:55:14 -0400

    Manual upload of updated inflow script

--- Scripts/Inflow_Aggregation.R
+++ Scripts/Inflow_Aggregation.R
@@ -1,15 +1,15 @@
 # Script to pull in FCR Inflow data from multiple years ####
 
-####QAQC Inflow
+#### QAQC Inflow
 ##Tasks
-#1. Compare to chemistry samples to see if dates line up, fix dates
+#1. Fix dates, aggregate all data
 #2. Plot to check for outliers
 #3. Identify missing data; end of 2017, before 2014
 #metadata tasks:
 #4. Figure out what the units are..
 #5. Identify start and end times
 #6. Should the raw data be included? as in inflow as pressure? Should we include stream dimensions/equations separately?
-#7. Should reservoir column still be in it, even though it is not relevant in this set?
+#7. Should reservoir column/site number still be in it, even though it is not relevant in this set?
 
 #install.packages('pacman') #installs pacman package, making it easier to load in packages
 pacman::p_load(tidyverse, lubridate, magrittr, ggplot2) #installs and loads in necessary packages for script
@@ -53,35 +53,52 @@ inflow <- pressure
 #################################################################################################
 
 
-#inflow$Reservoir <- "FCR" #creates reservoir column to match other data sets
-#inflow$Site <- 100  
-  FLOWS <- cbind(inflow,flow8,flow9,flow10, deparse.level = 1)
+inflow$Reservoir <- "FCR" #creates reservoir column to match other data sets
+inflow$Site <- 100  #creates site column to match other data sets
+  FLOWS <- cbind(inflow,flow8,flow9,flow10, deparse.level = 1) #binds relevant columns, copied from RPM's code, so idk what "deparse" is
   
   FLOWS <- FLOWS %>%
     # Rename columns if needed (TargetName = OriginalName)
     rename(Flow_Ls = flow8, Flow_cmm = flow9, Flow_cms = flow10)
   
 Inflow_Final <- FLOWS[,c(4,5,3,2,1,6:8)]
+Inflow_Final <- Inflow_Final[order(Inflow_Final$DateTime),] #orders file by date
 
 # Write to CSV
-write.csv(Inflow_Final, './Formatted_Data/inflow.csv', row.names=F)
+write.csv(Inflow_Final, './Formatted_Data/inflow.csv', row.names=F) #this would not push to github?? did manual upload
 
-
+#### MISCELLANEOUS TEST CODE #####
 ###Files that are broken
 #101416
 
+## Identifies Missing dates
+testtime=Inflow_Final[,c(3,4)] #pulls out datetime and temp, bc I hate dealing with vectors rn sry
 
-## Trying to check on stuff
+testtime2=testtime
+testtime2$Date <- as.Date(testtime2$DateTime) #changes DateTime to Date only
 
-for (i is 1 in nrow())
-  for
+#creates data frame of start and end of missing data
+Inflow_stopdates=data_frame(start = testtime2$Date[ diff(testtime2$Date)>1],
+                            end = testtime2$Date[-1] [ diff(testtime2$Date)>1]) 
+#note: only checks within parameters of existing data, so if your missing head and tail data, you are out of luck
 
-x11()
-ggplot(data = pressure)+
-  geom_line(aes(DateTime,Pressure_unit))
+##Trying to compare to FCR hourly inflow data
+#some of this code is garbage and doesn't work yet
+#need 2013-06 to 2014-06; 2017-08 to 2017-12
 
-testtime=inflow[c(1:30),c(3,4)]
-testtime$diff=NA
-for (i in 1:nrow(testtime)) {
-diff[i]=difftime(testtime$DateTime[i],testtime$DateTime[i+1])
-}
+daily_inflow <- dir(path = "./Data", pattern = "*Inflow.csv") %>% 
+  map_df(~ read_csv(file.path(path = "./Data", .), col_types = cols(.default = "c"))) %>% 
+   #limits data to necessary columns
+
+daily_inflow <- daily_inflow %>%
+  mutate(Date = ymd_hms(Date)) %>%
+  mutate_if(is.character,funs(as.double(.))) %>%   # parse all other columns to numeric
+
+  Date_daily= as.Date(daily_inflow$Date)   
+
+Date_15min=unique(testtime2$Date)
+
+# Get the dates in DateRange that are not in DF$V2 
+Date_daily[!Date_daily %in% Date_15min]
+
+daily_inf <- daily_inf[order(daily_inf$Date),] 
