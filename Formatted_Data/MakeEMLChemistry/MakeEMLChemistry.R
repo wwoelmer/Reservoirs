1fd509dff2851ca4368ff452989db19288f10208
melofton <melofton@vt.edu>
2018-05-24 16:27:34 -0400

    working on EML

---
+++ Formatted_Data/MakeEMLChemistry/MakeEMLChemistry.R
@@ -0,0 +1,56 @@
+##MakeEMLChemistry
+##Author: Mary Lofton
+##Date: 24MAY18
+
+# # Install devtools
+# install.packages("devtools")
+# 
+# # Load devtools
+# library(devtools)
+# 
+# # Install and load EMLassemblyline
+# install_github("EDIorg/EMLassemblyline")
+library(EMLassemblyline)
+
+#Step 1: Create a directory for your dataset
+#in this case, our directory is Reservoirs/Formatted_Data/MakeEMLChemistry
+
+#Step 2: Move your dataset to the directory
+
+#Step 3: Create an intellectual rights license
+#ours is CCBY
+
+#Step 4: Identify the types of data in your dataset
+#right now the only supported option is "table"
+
+#Step 5: Import the core metadata templates
+
+# View documentation for this function
+?import_templates
+
+# Import templates for an example dataset licensed under CC0, with 2 tables.
+import_templates(path = "C:/Users/Mary Lofton/Documents/RProjects/Reservoirs/Formatted_Data/MakeEMLChemistry",
+                 license = "CCBY",
+                 data.files = c("chemistry_manual_flags"))
+
+#Step 6: Script your workflow
+#that's what this is, silly!
+
+#Step 7: Abstract
+#copy-paste the abstract from your Microsoft Word document into abstract.txt
+#if you want to check your abstract for non-allowed characters, go to:
+#https://pteo.paranoiaworks.mobi/diacriticsremover/
+#paste text and click remove diacritics
+
+#Step 8: Methods
+#copy-paste the methods from your Microsoft Word document into abstract.txt
+#if you want to check your abstract for non-allowed characters, go to:
+#https://pteo.paranoiaworks.mobi/diacriticsremover/
+#paste text and click remove diacritics
+
+#Step 9: Additional information
+#nothing mandatory for Carey Lab in this section
+
+#Step 10: Keywords
+#DO NOT EDIT KEYWORDS FILE USING A TEXT EDITOR!! USE EXCEL!!
+
