1788b288e83402b3d67d6c165e9a99f729aef393
bethany.bookout <bethanyb18@vt.edu>
2018-05-25 10:25:40 -0400

    intermediate file that may need to be updated later

--- Formatted_Data/MakeEMLInflow/FCRInflowEML.R
+++ Formatted_Data/MakeEMLInflow/FCRInflowEML.R
@@ -5,15 +5,12 @@ pacman::p_load(devtools) #installs and loads to use install_github function
 install_github("EDIorg/EMLassemblyline")
 library(EMLassemblyline)
 
-### Step 1: set working directory ###
-setwd("~/Reservoirs/Formatted_Data/MakeEMLInflow/")
 
 ### Step 5: load templates ###
 # View documentation for this function
 #?import_templates
 
 # Import templates for an example dataset licensed under CC0, with 2 tables.
-import_templates(path = "",
-                 license = "CC0",
-                 data.files = c("lake_chloride_concentrations",
-                                "lake_characteristics"))
+import_templates(path = "~/Reservoirs/Formatted_Data/MakeEMLInflow/",
+                 license = "CCBY",
+                 data.files = "inflow")
