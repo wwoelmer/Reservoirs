728035277ff8e23881b41a6c5cacc838334a8902
bethanybookout <38041971+bethanybookout@users.noreply.github.com>
2018-05-25 14:02:20 -0400

    cleaned inflow aggregation

--- Scripts/Inflow_Aggregation.R
+++ Scripts/Inflow_Aggregation.R
@@ -2,9 +2,8 @@
 
 #### QAQC Inflow
 ##Tasks
-#1. Fix dates, aggregate all data
-#2. Plot to check for outliers
-#3. Identify missing data; end of 2017, before 2014
+#1. Plot to check for outliers
+#2. Fine tune QA/QC
 
 #install.packages('pacman') #installs pacman package, making it easier to load in packages
 pacman::p_load(tidyverse, lubridate, magrittr, ggplot2) #installs and loads in necessary packages for script
@@ -13,29 +12,27 @@ setwd("~/Reservoirs") #just in case your working directory is wonky
 ##Data from pressure transducer
 # Load in files with names starting with FCR_inf_15min, should only be .csv files
 raw_inflow <- dir(path = "./Data", pattern = "*FCR_inf_15min.csv") %>% 
-  map_df(~ read_csv(file.path(path = "./Data", .), col_types = cols(.default = "c"), skip = 37)) %>% 
+  map_df(~ read_csv(file.path(path = "./Data", .), col_types = cols(.default = "c"), skip = 37))
 raw_inf= raw_inflow[,c(6,3,4)] #limits data to necessary columns
 
 ###Inflow data must be paired with barometric data, this is an attempt to convert that.. maybe disregard
 
 pressure <- raw_inf %>%
   # Rename columns if needed (TargetName = OriginalName)
-  rename(Pressure_psia = "Pressure(in H2O)", DateTime = "Barometric Date/Time", Temp_C = "Temperature(degC)") %>%
+  rename(Pressure_psia = "Pressure(in H2O)", DateTime = "Barometric Date/Time", Temp_C = "Temperature(degC)")
 
 pressure=distinct(pressure) #removes duplicates
-
-#pressure$date <- as_datetime(pressure$DateTime)
 
    # Parse columns to target format
   pressure <- pressure %>%
   mutate(DateTime = ymd_hms(DateTime)) %>%
-  mutate_if(is.character,funs(as.double(.))) %>%   # parse all other columns to numeric
+  mutate_if(is.character,funs(as.double(.)))   # parse all other columns to numeric
   
   
 inflow <- pressure
-  flow2 <- pressure$Pressure_psia
+  flow2 <- pressure$Pressure_psia #creates vector to fit in with 'old school' script
   
-  ### CALCULATE THE FLOW RATES AT INFLOW ### #Taken from RPM's 'old school' script
+  ### CALCULATE THE FLOW RATES AT INFLOW ### #Taken from RPM's 'old school' script, verified 05-23-18
   #################################################################################################
   flow3 <- (flow2 - 6.3125 + 1.2) * 0.0254                     # Response: Height above the weir (m), rating curve equation
   flow4 <- (0.62 * (2/3) * (1.1) * 4.43 * (flow3 ^ 1.5) * 35.3147) # Flow CFS
@@ -56,49 +53,31 @@ inflow$Site <- 100  #creates site column to match other data sets
     # Rename columns if needed (TargetName = OriginalName)
     rename(Flow_cms = flow10)
   
-Inflow_Final <- FLOWS[,c(4,5,1,3,2,6)]
+Inflow_Final <- FLOWS[,c(4,5,1,3,2,6)] #orders columns
 Inflow_Final <- Inflow_Final[order(Inflow_Final$DateTime),] #orders file by date
+Inflow_Final <- Inflow_Final[-c(1,which(Inflow_Final$DateTime>"2017-12-31 23:45:00")),] #limits data to before 2017 and takes out first row erroneous value
 
 # Write to CSV
-write.csv(Inflow_Final, './Formatted_Data/inflow.csv', row.names=F) #this would not push to github?? did manual upload
+write.csv(Inflow_Final, './Formatted_Data/MakeEMLInflow/inflow.csv', row.names=F) #this would not push to github?? did manual upload
 
-#### MISCELLANEOUS TEST CODE #####
-###Files that are broken
-#101416
-library(lubridate)
+
+####### MISCELLANEOUS TEST CODE #########
 
 ## Identifies Missing dates
-testtime=Test_pressure[,c(2,3)] #pulls out datetime and temp, bc I hate dealing with vectors rn sry
+testtime=Inflow_Final[,c(2,3)] #pulls out datetime and temp, bc I hate dealing with vectors rn sry
 
 testtime2=testtime
-testtime2$Date=testtime2$BDateTime
-testtime2$Date <- as_date(testtime2$BDateTime) #changes DateTime to Date only
-testtime2=testtime2[order(testtime2$BDateTime),]
-#creates data frame of start and end of missing data
+testtime2$Date=testtime2$DateTime
+testtime2$Date <- as_date(testtime2$DateTime) #changes DateTime to Date only
+testtime2=testtime2[order(testtime2$DateTime),]
+#creates data frame of start and end of missing data by day
 Test_stopdates=data_frame(start = testtime2$Date[ diff(testtime2$Date)>1],
-                            end = testtime2$Date[-1] [ diff(testtime2$Date)>1]) 
+                            end = testtime2$Date[-1] [ diff(testtime2$Date)>1])
+#creates data frame of start and end of missing data, 15 min interval
+Test_stop15=data_frame(start15 = testtime2$DateTime[ diff(testtime2$DateTime)>900], end = testtime2$DateTime[-1] [ diff(testtime2$DateTime)>900])
 #note: only checks within parameters of existing data, so if your missing head and tail data, you are out of luck
 
-##Trying to compare to FCR hourly inflow data
-#some of this code is garbage and doesn't work yet
-#need 2013-06 to 2014-06; 2017-08 to 2017-12
+#write.csv(Test_stop15[c(73:88),], './Formatted_Data/MakeEMLInflow/Missing15minIntervals_Inflow.csv', row.names = F) #created data set of collection dates
 
-daily_inflow <- dir(path = "./Data", pattern = "*Inflow.csv") %>% 
-  map_df(~ read_csv(file.path(path = "./Data", .), col_types = cols(.default = "c"))) %>% 
-   #limits data to necessary columns
-
-daily_inflow <- daily_inflow %>%
-  mutate(Date = ymd_hms(Date)) %>%
-  mutate_if(is.character,funs(as.double(.))) %>%   # parse all other columns to numeric
-
-  Date_daily= as.Date(daily_inflow$Date)   
-
-Date_15min=unique(testtime2$Date)
-
-# Get the dates in DateRange that are not in DF$V2 
-Date_daily[!Date_daily %in% Date_15min]
-
-daily_inf <- daily_inf[order(daily_inf$Date),] 
-
-#Time check
-raw_inflow$`Date/Time`==as_date()
+head(Inflow_Final)
+tail(Inflow_Final)
