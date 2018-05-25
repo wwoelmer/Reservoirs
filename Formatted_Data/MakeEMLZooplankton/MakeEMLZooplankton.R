3138deefb8280d29c4bf59e6d1cf97790465ccc9
cayelan <cayelan@vt.edu>
2018-05-25 10:02:28 -0400

    added annotation for workflow

--- Formatted_Data/MakeEMLZooplankton/MakeEMLZooplankton.R
+++ Formatted_Data/MakeEMLZooplankton/MakeEMLZooplankton.R
@@ -18,18 +18,19 @@ data<-read.csv('zooplankton.csv', header=TRUE)
 View(data)
 
 import_templates(path = "~/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Formatted_Data/MakeEMLZooplankton",
-                 license = "CCBY",
-                 data.files = c("zooplankton"))
+                 license = "CCBY", #use CCBY instead of CCBO so that data users need to cite our package
+                 data.files = c("zooplankton")) #csv file name
 
 define_catvars(path = "~/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Formatted_Data/MakeEMLZooplankton")
 
 make_eml(path = "/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Formatted_Data/MakeEMLZooplankton",
          dataset.title = "Crustacean zooplankton density and biomass and rotifer density for Beaverdam Reservoir, Carvins Cove Reservoir, Gatewood Reservoir, and Spring Hollow Reservoir in southwestern Virginia, USA 2014-2016",
          data.files = c("zooplankton"),
-         data.files.description = c("Reservoir zooplankton dataset"),
+         data.files.description = c("Reservoir zooplankton dataset"), #short title
          data.files.quote.character = c("\""),
          temporal.coverage = c("2014-04-04", "2016-10-25"),
          geographic.description = "Southwestern Virginia, USA, North America",
          maintenance.description = "completed", 
-         user.id = "carylab0",
-         package.id = "edi.198.1")
+         user.id = "carylab0", #your personal ID, will be Carey Lab ID eventually!
+         package.id = "edi.198.1") #from EDI portal, login, and then reserve a package ID via the
+          #Data Package Identifier Reservations
