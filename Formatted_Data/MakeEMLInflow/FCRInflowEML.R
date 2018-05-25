ab2604d5b09d22cb728b982bed14f71be8d0d70e
bethany.bookout <bethanyb18@vt.edu>
2018-05-25 11:15:15 -0400

    magic..

--- Formatted_Data/MakeEMLInflow/FCRInflowEML.R
+++ Formatted_Data/MakeEMLInflow/FCRInflowEML.R
@@ -10,7 +10,31 @@ library(EMLassemblyline)
 # View documentation for this function
 #?import_templates
 
+<<<<<<< HEAD
 # Import templates for an example dataset licensed under CC0, with 2 tables.
 import_templates(path = "~/Reservoirs/Formatted_Data/MakeEMLInflow/",
                  license = "CCBY",
                  data.files = "inflow")
+=======
+# Import templates for an example dataset licensed under CCBY, with 2 tables.
+import_templates(path = "",
+                 license = "CCBY",
+                 data.files = c("inflow"))
+
+data<-read.csv('inflow.csv', header=TRUE)
+View(data)
+
+
+define_catvars(path = "")
+
+make_eml(path = "",
+         dataset.title = "Discharge time series for the primary inflow tributary entering Falling Creek Reservoir, Vinton, Virginia, USA 2013-2017",
+         data.files = c("inflow"),
+         data.files.description = c("FCR inflow dataset"),
+         data.files.quote.character = c("\""),
+         temporal.coverage = c("2013-05-13", "2017-12-31"),
+         geographic.description = "Southwestern Virginia, USA, North America",
+         maintenance.description = "ongoing", 
+         user.id = "carylab0",
+         package.id = "edi.198.1")
+>>>>>>> 4d05a77487e9bdcc28a2a1c49914900fbc72cb6d
