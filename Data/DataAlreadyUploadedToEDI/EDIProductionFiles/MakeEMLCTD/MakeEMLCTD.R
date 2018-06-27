dc01dd01db39f2e5421b704a4173f17ce17d8ab4
Colin Smith <colin.smith@wisc.edu>
2018-06-27 10:11:52 -0500

    Make EML for data package revision edi.200.2

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD/MakeEMLCTD.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD/MakeEMLCTD.R
@@ -15,13 +15,15 @@ import_templates(path = "C:/Users/Owner/Desktop/EDI_CTD_upload",
 define_catvars(path = "C:/Users/Owner/Desktop/EDI_CTD_upload")
 
 # Make the EML for EDI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-make_eml(path = "C:/Users/Owner/Desktop/EDI_CTD_upload",
+path = "C:/Users/Owner/Desktop/EDI_CTD_upload"
+
+make_eml(path = "C:\\Users\\Colin\\Documents\\EDI\\data_sets\\Reservoirs\\Data\\DataAlreadyUploadedToEDI\\EDIProductionFiles\\MakeEMLCTD",
          dataset.title = "Time series of high-frequency profiles of depth, temperature, dissolved oxygen, conductivity, specific conductivity, chlorophyll a, turbidity, pH, and oxidation-reduction potential for Beaverdam Reservoir, Carvins Cove Reservoir, Falling Creek Reservoir, Gatewood Reservoir, and Spring Hollow Reservoir in Southwestern Virginia, USA 2013-2017",
-         data.files = "CTD_Meta_13_17",
+         data.files = "CTD_Meta_13_17.csv",
          data.files.description = "Reservoir CTD dataset",
          data.files.quote.character = "\"",
          temporal.coverage = c("2013-03-07", "2017-12-10"),
-         geographic.description = "Southwestern Virginia, USA, North America",
+         #geographic.description = "Southwestern Virginia, USA, North America", # This argument is only required if bounding_boxes.txt is absent.
          maintenance.description = "ongoing", 
          user.id = "carylab7",
-         package.id = "edi.200.1")
+         package.id = "edi.200.2")
