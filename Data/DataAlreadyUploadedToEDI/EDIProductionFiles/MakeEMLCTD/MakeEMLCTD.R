e0b1d18e249510bb455ad30b4c4388c8af6181d6
cayelan <cayelan@vt.edu>
2018-07-02 15:48:21 -0400

    edited R script paths to run on my machine

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD/MakeEMLCTD.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD/MakeEMLCTD.R
@@ -1,3 +1,5 @@
+setwd("/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD")
+
 # Install and load devtools
 install.packages("devtools")
 library(devtools)
@@ -7,17 +9,17 @@ install_github("EDIorg/EMLassemblyline")
 library(EMLassemblyline)
 
 # Import Templates ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import_templates(path = "C:/Users/Owner/Desktop/EDI_CTD_upload", 
+import_templates(path = "/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD", 
                  license = "CCBY", 
-                 data.files = "CTD_Meta_13_17")
+                 data.files = c("CTD_Meta_13_17"))
 
 # Define Categorical Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-define_catvars(path = "C:/Users/Owner/Desktop/EDI_CTD_upload")
+define_catvars(path = "/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD")
 
 # Make the EML for EDI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-path = "C:/Users/Owner/Desktop/EDI_CTD_upload"
+#path = "C:/Users/Owner/Desktop/EDI_CTD_upload"
 
-make_eml(path = "C:\\Users\\Colin\\Documents\\EDI\\data_sets\\Reservoirs\\Data\\DataAlreadyUploadedToEDI\\EDIProductionFiles\\MakeEMLCTD",
+make_eml(path = "/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/Falling Creek/DataForWebsite/Github/ReservoirData/Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD",
          dataset.title = "Time series of high-frequency profiles of depth, temperature, dissolved oxygen, conductivity, specific conductivity, chlorophyll a, turbidity, pH, and oxidation-reduction potential for Beaverdam Reservoir, Carvins Cove Reservoir, Falling Creek Reservoir, Gatewood Reservoir, and Spring Hollow Reservoir in Southwestern Virginia, USA 2013-2017",
          data.files = "CTD_Meta_13_17.csv",
          data.files.description = "Reservoir CTD dataset",
@@ -26,4 +28,4 @@ make_eml(path = "C:\\Users\\Colin\\Documents\\EDI\\data_sets\\Reservoirs\\Data\\
          #geographic.description = "Southwestern Virginia, USA, North America", # This argument is only required if bounding_boxes.txt is absent.
          maintenance.description = "ongoing", 
          user.id = "carylab7",
-         package.id = "edi.200.2")
+         package.id = "edi.200.3")
